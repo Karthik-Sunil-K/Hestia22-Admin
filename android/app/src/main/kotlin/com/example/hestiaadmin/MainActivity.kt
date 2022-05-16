cf16507f7c7608021dae39f34db9374f58bec0b3
Karthik-Sunil-K <karthiksunil038@gmail.com>
2022-05-16 21:35:27 +0530

    first commit

---
+++ android/app/src/main/kotlin/com/example/hestiaadmin/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.hestiaadmin
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,81 @@
+import 'package:flutter/material.dart';
+import 'package:hestiaadmin/screens/bottomnavigation/navbar.dart';
+
+import 'screens/login/login.dart';
+// import 'package:webview_flutter/webview_flutter.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Hestia22',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    double screenHeight = MediaQuery.of(context).size.height;
+    double screenWidth = MediaQuery.of(context).size.width;
+
+    // TODO: Replace with provider once backend is up
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.all(5.0),
+        child: SizedBox(
+          height: screenHeight,
+          width: screenWidth,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              ElevatedButton(
+                  onPressed: () {
+                    Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => const NavBar()));
+                  },
+                  child: const Text("home")),
+              ElevatedButton(
+                  onPressed: () {
+                    Navigator.push(context,
+                        MaterialPageRoute(builder: (context) => const Login()));
+                  },
+                  child: const Text("login")),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class Constants {
+  static const Color color1 = Colors.black;
+  static const Color color2 = Colors.white;
+
+  static const TextStyle primaryText = TextStyle();
+  static const TextStyle secondaryText = TextStyle();
+  static const TextStyle tertiaryText = TextStyle();
+
+}
